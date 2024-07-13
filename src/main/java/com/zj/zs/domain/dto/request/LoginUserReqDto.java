f072e3d00d3786f715bf5c880bbe23285188b527
zhoujun134 <1161386101@qq.com>
2024-07-13 22:23:33 +0800

    fix code

--- src/main/java/com/zj/zs/domain/dto/request/LoginUserReqDto.java
+++ src/main/java/com/zj/zs/domain/dto/request/LoginUserReqDto.java
@@ -1,6 +1,7 @@
 package com.zj.zs.domain.dto.request;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -15,8 +16,10 @@ import javax.validation.constraints.NotBlank;
 public class LoginUserReqDto {
 
     @NotBlank(message = "用户名不能为空！")
+    @Length(min = 6, max = 20, message = "用户名长度在6-20之间！")
     private String username;
 
     @NotBlank(message = "密码不能为空！")
+    @Length(min = 8, max = 20, message = "密码长度在8-20之间！")
     private String password;
 }
--- src/main/java/com/zj/zs/domain/dto/request/RegisterUserInfoReqDto.java
+++ src/main/java/com/zj/zs/domain/dto/request/RegisterUserInfoReqDto.java
@@ -1,6 +1,7 @@
 package com.zj.zs.domain.dto.request;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -14,15 +15,19 @@ import javax.validation.constraints.NotBlank;
 @Data
 public class RegisterUserInfoReqDto {
 
+    @NotBlank(message = "姓名不能为空")
     private String name;
 
     @NotBlank(message = "用户名不能为空")
+    @Length(min = 6, max = 20, message = "用户名长度在6-20之间！")
     private String username;
 
     @NotBlank(message = "密码不能为空")
+    @Length(min = 8, max = 20, message = "密码长度在8-20之间！")
     private String password;
 
     @NotBlank(message = "邮箱不能为空")
+    @Length(min = 3, max = 4, message = "邮箱长度在6-20之间！")
     private String email;
 
     private String description;
